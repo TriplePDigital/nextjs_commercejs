2f4b75dc90f2c8e56e839dc581205654a849d23e
Daniel Papp <dpapp001@odu.edu>
2022-03-16 18:34:02 -0400

    💄 (UI): matched course page to mockup
    The page now looks 75% like the mockup. Still missing functionality. Not fetching from Strapi yet.

--- components/Nav/Navbar.js
+++ components/Nav/Navbar.js
@@ -8,47 +8,80 @@ export default function Navbar() {
 	return (
 		<nav className={`w-full h-16 border-b border-gray-200 px-10`}>
 			<ul className={`flex flex-row justify-between items-center h-full`}>
-				<ActiveLink href={`/`} activeClassName="font-bold">
-					<a>Home</a>
-				</ActiveLink>
-				<ActiveLink href={`/products`} activeClassName="font-bold">
-					<a>Products</a>
-				</ActiveLink>
-				{/* <ActiveLink href={`/abc/orders`} activeClassName="font-bold">
-					<a>Orders</a>
-				</ActiveLink> */}
-				<ActiveLink href={`/missions`} activeClassName="font-bold">
-					<a>Missions</a>
-				</ActiveLink>
-				{!session ? (
-					<button onClick={() => signIn()}>Sign in</button>
-				) : loading ? (
-					<Loader />
-				) : (
-					<div className="bg-blue-300 rounded-lg px-5 py-2 flex flex-row items-center">
-						<div className="h-8 w-8 rounded-full mr-2 overflow-hidden">
-							<Image
-								src={session?.user?.image}
-								alt="Profile picture"
-								width={4}
-								height={4}
-								layout="responsive"
-								quality={50}
-							/>
-						</div>
-						<span
-							className={`inline-block capitalize mr-2 font-semibold leading-loose tracking-wide`}
-						>
-							{session.user.name.toLowerCase()}!
-						</span>
+				<div className="flex flex-row justify-between items-center h-full w-1/2">
+					<ActiveLink
+						href={`/`}
+						activeClassName="font-bold underline underline-offset-8"
+					>
+						<a className="border-b-2 border-transparent">Home</a>
+					</ActiveLink>
+					<ActiveLink
+						href={`/my-learning`}
+						activeClassName="font-bold underline underline-offset-8"
+					>
+						<a>My Learning</a>
+					</ActiveLink>
+					<ActiveLink
+						href={`/missions`}
+						activeClassName="font-bold underline underline-offset-8"
+					>
+						<a>Courses</a>
+					</ActiveLink>
+					<ActiveLink
+						href={`/certs`}
+						activeClassName="font-bold underline underline-offset-8"
+					>
+						<a>Certificates</a>
+					</ActiveLink>
+				</div>
+				<div className="w-1/3 flex justify-end">
+					{!session ? (
 						<button
-							className="underline cursor-pointer"
-							onClick={() => signOut()}
+							className="bg-teal-400 font-bold text-white uppercase px-6 py-2 rounded leading-loose tracking-wide"
+							onClick={() => signIn()}
 						>
-							Sign out
+							Sign in
 						</button>
-					</div>
-				)}
+					) : loading ? (
+						<Loader />
+					) : (
+						<>
+							<button
+								className="bg-teal-400 font-bold text-white uppercase px-6 py-2 rounded leading-loose tracking-wide mr-12"
+								onClick={() =>
+									alert(
+										'Purchase membership to access exclusive features'
+									)
+								}
+							>
+								no membership
+							</button>
+							<div className="bg-teal-100 rounded-lg px-5 py-2 flex flex-row items-center">
+								<div className="h-8 w-8 rounded-full mr-2 overflow-hidden">
+									<Image
+										src={session?.user?.image}
+										alt="Profile picture"
+										width={4}
+										height={4}
+										layout="responsive"
+										quality={50}
+									/>
+								</div>
+								<span
+									className={`inline-block capitalize mr-2 font-semibold leading-loose tracking-wide`}
+								>
+									{session.user.name.toLowerCase()}!
+								</span>
+								{/* <button
+								className="underline cursor-pointer"
+								onClick={() => signOut()}
+							>
+								Sign out
+							</button> */}
+							</div>
+						</>
+					)}
+				</div>
 			</ul>
 		</nav>
 	)
