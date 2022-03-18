16a942044aada974955c1cb81baaa2df867ef301
Daniel Papp <dpapp001@odu.edu>
2022-03-18 15:08:39 -0400

    💄 (styles): Created branded color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,23 @@ module.exports = {
 	],
 	darkMode: false, // or 'media' or 'class'
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				ncrma: {
+					DEFAULT: '#5FBBBE',
+					50: '#E2F2F3',
+					100: '#D3ECED',
+					200: '#B6E0E1',
+					300: '#99D4D6',
+					400: '#7CC7CA',
+					500: '#5FBBBE',
+					600: '#42A0A3',
+					700: '#32797B',
+					800: '#225153',
+					900: '#122A2B'
+				}
+			}
+		}
 	},
 	variants: {
 		extend: {
