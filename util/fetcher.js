fa06e11942d0a18909856f838b74417284950fed
Daniel Papp <dpapp001@odu.edu>
2022-04-07 11:41:08 -0400

    ✨ (utils): Modified utility functions
    Updated the sanity image constructor utility function to handle errors better. Introduced caching to the fetching of Sanity data

--- util/fetcher.js
+++ util/fetcher.js
@@ -1,11 +1,25 @@
 import axios from 'axios'
 
-export const fetcher = async (query) => {
-	const results = await axios.get(
-		`https://tfh7h5l0.api.sanity.io/v1/data/query/production?query=${encodeURIComponent(
-			query
-		)}`
-	)
+const date = new Date()
 
-	return results.data.result
+export const fetcher = async (query) => {
+	try {
+		const results = await axios.get(
+			`https://tfh7h5l0.api.sanity.io/v1/data/query/production?query=${encodeURIComponent(
+				query
+			)}`,
+			{
+				headers: {
+					'Cache-Control': 'max-age=31536000, immutable',
+					'If-Modified-Since': `${date.setMinutes(
+						date.getMinutes() - 120
+					)}`
+				}
+			}
+		)
+
+		return results.data.result
+	} catch (error) {
+		throw new Error(error)
+	}
 }
