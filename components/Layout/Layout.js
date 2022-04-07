769b880208c488dbb6a20016186082c45f1a6299
Daniel Papp <dpapp001@odu.edu>
2022-04-07 11:43:48 -0400

    💄 (UI): Created lesson page

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,12 +1,31 @@
 import { Navbar } from '../Nav'
 import { Footer } from './'
+import { useSession } from 'next-auth/client'
+import { useRouter } from 'next/router'
 
 export default function Layout({ children }) {
+	const [session, loading] = useSession()
+	const router = useRouter()
+	console.log(router)
+	console.log(session)
 	return (
 		<>
-			<Navbar />
-			<main className="mb-10 px-10">{children}</main>
+			{router?.pathname.includes('login') ? null : <Navbar />}
+			<main
+				className={`mb-10 px-10 ${
+					router?.pathname.includes('login') ? 'h-screen' : ''
+				}`}
+			>
+				{children}
+			</main>
 			{/* <Footer /> */}
 		</>
 	)
+}
+
+export async function getServerSideProps(ctx) {
+	// const res = await fetcher(`
+	// *[_type == 'explorer' && email == '${session?.user?.email}']{
+	//     ...,
+	// }`)
 }
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,25 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import qs from 'qs'
+import { Loader } from '../components/util'
+import { fetcher } from '../util/fetcher'
+import { useSession, signIn, signOut } from 'next-auth/client'
 
-export default function Home() {
-	return (
-		<>
-			<h1>Home page</h1>
-		</>
-	)
+export default function Home({ missions }) {
+	const [session, loading] = useSession()
+
+	if (session) {
+		return (
+			<div>
+				<p>User: {session.user.name}</p>
+				<button onClick={() => signOut({ redirect: false })}>
+					Sign Out
+				</button>
+			</div>
+		)
+	}
+
+	return <button onClick={() => signIn('google')}>Sign In</button>
 }
