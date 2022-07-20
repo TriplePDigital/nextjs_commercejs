ea7390498915fea2de8f55ced7e86a69687a848c
Dániel B. Papp <dpapp@odu.edu>
2022-07-20 15:33:50 -0400

    fix(mission): :sparkles: Created a landing page for not-enrolled courses
    Students who do not have an enrollment for a given course but the course's content is already on the platform are now shown a basic intro into the course along with markdown enabled course description.

--- pages/mission/[slug]/index.js
+++ pages/mission/[slug]/index.js
@@ -11,6 +11,14 @@ import Stages from '@/components/Course/Stages'
 import isVideo from '@/util/isVideo'
 import getUserFromSession from '@/util/getUserFromSession'
 import getEnrollmentByStudentIDandCourseID from '@/util/getEnrollmentByStudentIDandCourseID'
+import { MdOutlineAssignment } from 'react-icons/md'
+import { AiFillStar, AiOutlineClockCircle } from 'react-icons/ai'
+import { FaBook } from 'react-icons/fa'
+import { BsTagsFill } from 'react-icons/bs'
+import { RiBook2Line, RiDoubleQuotesL } from 'react-icons/ri'
+import ReactMarkdown from 'react-markdown'
+import mdConfig from '@/util/md'
+import Link from 'next/link'
 
 // const getLatestProgress = (stages) => {
 // 	let progress = []
@@ -50,14 +58,20 @@ function MissionSlug({ session, mission, user, enrollment }) {
 	const [stageContext, setStageContext] = useState(0)
 	const [checkpointContext, setCheckpointContext] = useState(0)
 	const [currentCheckpoint, setCurrentCheckpoint] = useState(
-		enrollment.course.stages[stageContext].checkpoints[checkpointContext]
+		enrollment
+			? enrollment.course.stages[stageContext].checkpoints[
+					checkpointContext
+			  ]
+			: null
 	)
 
 	useEffect(() => {
 		setCurrentCheckpoint(
-			enrollment.course.stages[stageContext].checkpoints[
-				checkpointContext
-			]
+			enrollment
+				? enrollment.course.stages[stageContext].checkpoints[
+						checkpointContext
+				  ]
+				: null
 		)
 
 		setLoading(false)
@@ -129,10 +143,9 @@ function MissionSlug({ session, mission, user, enrollment }) {
 			}
 		}
 	}
-
 	return loading ? (
 		<Loader />
-	) : (
+	) : enrollment ? (
 		<div className="flex flex-row">
 			<div className="w-9/12 bg-gray-100 shadow-md border px-4 py-6 mt-6 mx-2 ml-0 rounded">
 				<>
@@ -159,30 +172,35 @@ function MissionSlug({ session, mission, user, enrollment }) {
 						/>
 					</div>
 					<div className="flex items-center my-6">
-						<a className="flex items-center mx-4 first:ml-0">
-							<div className="h-10 w-10 rounded-full overflow-hidden mr-2 relative">
-								{currentCheckpoint.type?.instructor ? (
-									<>
-										<Image
-											{...imgConstructor(
-												currentCheckpoint.type
-													?.instructor.avatar,
-												{
-													fit: 'fill'
-												}
-											)}
-											alt="Instructor Avatar"
-											layout="fill"
-											quality={50}
-										/>
-										<span className="absolute top-0 left-0 rounded-full h-full w-full bg-ncrma-300 opacity-50"></span>
-									</>
-								) : null}
+						<Link
+							href={`/user/instructor/${currentCheckpoint.type?.instructor._id}`}
+							passHref
+						>
+							<div className="flex items-center mx-4 first:ml-0">
+								<div className="h-10 w-10 rounded-full overflow-hidden mr-2 relative">
+									{currentCheckpoint.type?.instructor ? (
+										<>
+											<Image
+												{...imgConstructor(
+													currentCheckpoint.type
+														?.instructor.avatar,
+													{
+														fit: 'fill'
+													}
+												)}
+												alt="Instructor Avatar"
+												layout="fill"
+												quality={50}
+											/>
+											<span className="absolute top-0 left-0 rounded-full h-full w-full bg-ncrma-300 opacity-50"></span>
+										</>
+									) : null}
+								</div>
+								<span className="font-semibold leading-loose text-lg">
+									{currentCheckpoint.type?.instructor.name}
+								</span>
 							</div>
-							<span className="font-semibold leading-loose text-lg">
-								{currentCheckpoint.type?.instructor.name}
-							</span>
-						</a>
+						</Link>
 					</div>
 					<div>
 						<div className="font-semibold text-lg">
@@ -218,6 +236,146 @@ function MissionSlug({ session, mission, user, enrollment }) {
 				setCurrentCheckpoint={setCurrentCheckpoint}
 			/>
 		</div>
+	) : (
+		<div className="flex mx-auto w-full">
+			<div className="w-8/12 mr-8">
+				<h1 className="text-4xl leading-loose tracking-wide font-bold">
+					{mission.title}
+				</h1>
+				<h2 className="text-lg leading-loose tracking-wide font-medium text-gray-600">
+					{mission.blurb}
+				</h2>
+				<ul className="mt-2 flex">
+					{/* TODO: This is where we are going to create the course accordion */}
+				</ul>
+				<div className="relative h-full w-full mt-6">
+					{/* TODO: Either show cover image or preview video if available */}
+					<Image
+						{...imgConstructor(mission.coverImage, {
+							fit: 'fill'
+						})}
+						alt={mission.title}
+						objectFit="cover"
+						objectPosition="center"
+						quality={50}
+					/>
+				</div>
+				<h2 className="text-xl leading-loose tracking-wide font-bold mb-1 mt-6">
+					About The Instructors
+				</h2>
+				<div className="flex flex-row items-center justify-between mt-6">
+					{mission.instructors.map((instructor, index) => {
+						console.log(instructor)
+						return (
+							<div key={index} className="flex flex-col">
+								<div className="flex items-center gap-4">
+									<div className="relative w-14 h-14 rounded-full overflow-hidden">
+										<Image
+											{...imgConstructor(
+												instructor.avatar,
+												{
+													fit: 'fill'
+												}
+											)}
+											placeholder="blur"
+											alt="the instructors avatar image in the shape of a circle"
+										/>
+										<div className="rounded-full absolute top-0 left-0 h-full w-full bg-ncrma-500 opacity-40"></div>
+									</div>
+									<div className="flex flex-col gap-1 text-sm text-gray-700">
+										<span className="block font-medium">
+											{instructor.name}
+										</span>
+										<span className="block underline lowercase">
+											{instructor.email}
+										</span>
+									</div>
+								</div>
+								<ReactMarkdown
+									components={mdConfig}
+									className="my-2"
+								>
+									{instructor.bio}
+								</ReactMarkdown>
+							</div>
+						)
+					})}
+				</div>
+				<h2 className="text-xl leading-loose tracking-wide font-bold mb-1 mt-6">
+					About This Course
+				</h2>
+				<ReactMarkdown components={mdConfig}>
+					{mission?.description}
+				</ReactMarkdown>
+			</div>
+			<div className="w-4/12 p-5 bg-white border-l border-gray-200 flex flex-col gap-3">
+				{/* TODO: Add ability to give feedback on courses before tackling this... */}
+				{/* <div className=" flex items-center">
+					<AiFillStar className="text-yellow-500 mr-2" />
+					<span className="text-lg font-semibold mr-1">{5}.0</span>
+					<span className="text-gray-500">(1512 Reviews)</span>
+				</div> */}
+				<div className="text-gray-500 flex items-center gap-4">
+					<RiBook2Line className="mr-1" />
+					<div className="flex flex-col">
+						<span className="font-medium">Lessons</span>
+						<ol className="list-decimal">
+							{mission.stages.map((stage, index) => {
+								return (
+									<li
+										key={index}
+										className="text-sm text-gray-800 ml-6"
+									>
+										{stage.title}
+									</li>
+								)
+							})}
+						</ol>
+					</div>
+				</div>
+				<div className="text-gray-500 flex items-center gap-4">
+					<MdOutlineAssignment className="mr-1" />
+					<div className="flex flex-col">
+						<span className="font-medium">Chapters</span>
+						<ol className="list-decimal">
+							{mission.stages.map((stage) => {
+								return stage.checkpoints.map(
+									(checkpoint, j) => {
+										return (
+											<li
+												key={j}
+												className="text-sm text-gray-800 ml-6"
+											>
+												{checkpoint.title}
+											</li>
+										)
+									}
+								)
+							})}
+						</ol>
+					</div>
+				</div>
+				<div className="text-gray-500 flex items-center gap-4">
+					<AiOutlineClockCircle className="mr-1" />
+					2h 10m
+				</div>
+				<div className="text-gray-500 flex items-center gap-4">
+					<BsTagsFill className="mr-1" />
+					<div className="gap-2 flex">
+						{mission.categories.map((category, index) => {
+							return (
+								<span
+									key={index}
+									className="px-4 py-1 rounded-full bg-ncrma-600 hover:bg-ncrma-800 hover:shadow cursor-pointer block text-sm text-white"
+								>
+									{category}
+								</span>
+							)
+						})}
+					</div>
+				</div>
+			</div>
+		</div>
 	)
 }
 
@@ -227,17 +385,27 @@ export async function getServerSideProps(ctx) {
 	const session = await getSession(ctx)
 	const user = await getUserFromSession(session?.user?.email)
 	const mission = await getMissionBySlug(ctx.params.slug)
-	const enrollment = await getEnrollmentByStudentIDandCourseID(
-		user._id,
-		mission._id
-	)
-
-	return {
-		props: {
-			session,
-			mission,
-			user,
-			enrollment
+	try {
+		const enrollment = await getEnrollmentByStudentIDandCourseID(
+			user._id,
+			mission._id
+		)
+		return {
+			props: {
+				session,
+				mission,
+				user,
+				enrollment
+			}
+		}
+	} catch (error) {
+		return {
+			props: {
+				session,
+				mission,
+				user,
+				enrollment: null
+			}
 		}
 	}
 }
