4dfdac03d05ffb3f16a67261aace3f814d5fa502
Dániel B. Papp <dpapp@odu.edu>
2022-08-10 16:30:17 -0400

    feat: :sparkles: Configured toast notifications
    Now we can use the following notification types: success, info, error, and warning.

---
+++ components/util/Notification.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { toast } from 'react-toastify'
+import {
+	FaInfo,
+	FaCheck,
+	FaExclamationTriangle,
+	FaBug,
+	FaExclamationCircle
+} from 'react-icons/fa'
+
+export const displayIcon = (type) => {
+	switch (type) {
+		case 'success':
+			return <FaCheck />
+		case 'info':
+			return <FaInfo />
+		case 'error':
+			return <FaExclamationCircle />
+		case 'warning':
+			return <FaExclamationTriangle />
+		default:
+			return <FaBug />
+	}
+}
+
+const ToastMessage = ({ type, message, ID }) =>
+	toast[type](
+		<div style={{ display: 'flex' }}>
+			<div style={{ flexGrow: 1, fontSize: 15, padding: '8px 12px' }}>
+				{message}
+			</div>
+		</div>,
+		{
+			toastId: ID
+		}
+	)
+
+ToastMessage.propTypes = {
+	message: PropTypes.string.isRequired,
+	type: PropTypes.string.isRequired,
+	ID: PropTypes.string.isRequired
+}
+
+ToastMessage.dismiss = toast.dismiss
+
+export default ToastMessage
--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,16 @@
 import 'tailwindcss/tailwind.css'
+import 'react-toastify/dist/ReactToastify.css'
 import { Provider } from 'next-auth/client'
 import { Layout } from '../components/Layout'
 import { SWRConfig } from 'swr'
 import { createContext, useState } from 'react'
 import { fetcher } from '../util/fetcher'
+import { ToastContainer } from 'react-toastify'
 
 export const UserContext = createContext({
 	user: {},
 	setUser: () => {}
 })
-
-// const fetcher = (url) =>
-// 	axios
-// 		.get(url)
-// 		.then((res) => res.data)
-// 		.catch((err) => {
-// 			throw err
-// 		})
 
 function MyApp({ Component, pageProps }) {
 	const [user, setUser] = useState(null)
@@ -27,6 +21,16 @@ function MyApp({ Component, pageProps }) {
 				<SWRConfig value={{ fetcher }}>
 					<Layout session={pageProps.session}>
 						<Component {...pageProps} />
+						<ToastContainer
+							position="top-right"
+							autoClose={8000}
+							hideProgressBar={false}
+							newestOnTop={false}
+							draggable={false}
+							pauseOnVisibilityChange
+							closeOnClick
+							pauseOnHover
+						/>
 					</Layout>
 				</SWRConfig>
 			</UserContext.Provider>
