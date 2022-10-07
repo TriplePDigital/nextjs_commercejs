8b5e323944f613c418ace3743f8562a53011b77c
Dániel B. Papp <dpapp@odu.edu>
2022-10-06 21:22:42 -0400

    :bug:(MMP): Changes body field to read on upload

--- pages/api/admin/upload/index.js
+++ pages/api/admin/upload/index.js
@@ -1,6 +1,7 @@
 export default async function (req, res) {
-	const fileUpload = await req.body.file
-	if (!res) {
+	const fileUpload = await req.body.csv
+
+	if (!req.body) {
 		res.status(500).json({
 			error: 'There was an error processing your CSV file.'
 		})
