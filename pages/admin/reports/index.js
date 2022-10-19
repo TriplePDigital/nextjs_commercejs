2b77a5aa44e1a80bc7623b4df218835a6c021394
Dániel B. Papp <dpapp@odu.edu>
2022-10-18 22:25:19 -0400

    :sparkles: (MMP): Introduced DataTable dep Configured and tested the DataTable package with RMs data. Still needs a lot of work

--- pages/admin/reports/index.js
+++ pages/admin/reports/index.js
@@ -1,16 +1,16 @@
-import React, { useEffect, useRef, useState } from 'react'
-import { Table } from 'flowbite-react'
+import React, { useRef, useState } from 'react'
 import { getSession } from 'next-auth/client'
 import { fetcher } from '@/util/fetcher'
 import getUserFromSession from '@/util/getUserFromSession'
 import ProficiencyMatrix from '@/components/util/ProficiencyMatrix'
-import moment from 'moment'
-import { BsBuilding } from 'react-icons/bs'
 import { RiskManagementProfile } from '../../user/student/[id]'
+import { flexRender, getCoreRowModel, getExpandedRowModel, getFilteredRowModel, getPaginationRowModel, useReactTable } from '@tanstack/react-table'
+import { FaChevronDown, FaChevronRight } from 'react-icons/fa'
+import { GrClose } from 'react-icons/gr'
 
 const ReportingPage = ({ data, companyData }) => {
 	const [riskMangers, setRiskMangers] = useState(data)
-	const [expanded, setExpanded] = useState(null)
+	const [expanded, setExpanded] = useState({})
 	const [selectedCompany, setSelectedCompany] = useState([])
 	const [selectedFacility, setSelectedFacility] = useState(null)
 
@@ -67,7 +67,140 @@ const ReportingPage = ({ data, companyData }) => {
 		})
 	}
 
-	useEffect(() => {}, [])
+	const columns = React.useMemo(
+		() => [
+			{
+				header: 'Name',
+				footer: (props) => props.column.id,
+				columns: [
+					{
+						accessorKey: 'firstName',
+						header: ({ table }) => (
+							<>
+								<IndeterminateCheckbox
+									{...{
+										checked: table.getIsAllRowsSelected(),
+										indeterminate: table.getIsSomeRowsSelected(),
+										onChange: table.getToggleAllRowsSelectedHandler()
+									}}
+								/>{' '}
+								<button
+									{...{
+										onClick: table.getToggleAllRowsExpandedHandler()
+									}}
+								>
+									{table.getIsAllRowsExpanded() ? <FaChevronDown /> : <FaChevronRight />}
+								</button>{' '}
+								First Name
+							</>
+						),
+						cell: ({ row, getValue }) => (
+							<div
+								style={{
+									// Since rows are flattened by default,
+									// we can use the row.depth property
+									// and paddingLeft to visually indicate the depth
+									// of the row
+									paddingLeft: `${row.depth * 2}rem`
+								}}
+							>
+								<>
+									<IndeterminateCheckbox
+										{...{
+											checked: row.getIsSelected(),
+											indeterminate: row.getIsSomeSelected(),
+											onChange: row.getToggleSelectedHandler()
+										}}
+									/>{' '}
+									{row.getCanExpand() ? (
+										<button
+											{...{
+												onClick: row.getToggleExpandedHandler(),
+												style: { cursor: 'pointer' }
+											}}
+										>
+											{row.getIsExpanded() ? <FaChevronDown /> : <FaChevronRight />}
+										</button>
+									) : (
+										<GrClose />
+									)}{' '}
+									{getValue()}
+								</>
+							</div>
+						),
+						footer: (props) => props.column.id
+					},
+					{
+						accessorFn: (row) => row.lastName,
+						id: 'lastName',
+						cell: (info) => info.getValue(),
+						header: () => <span>Last Name</span>,
+						footer: (props) => props.column.id
+					},
+					{
+						accessorFn: (row) => row.email,
+						id: 'email',
+						cell: (info) => info.getValue(),
+						header: () => <span>Last Name</span>,
+						footer: (props) => props.column.id
+					}
+				]
+			},
+			{
+				header: 'Info',
+				footer: (props) => props.column.id,
+				columns: [
+					{
+						accessorKey: 'enrollments',
+						cell: (info) => Number(info.getValue().length),
+						header: () => 'Enrollments',
+						footer: (props) => props.column.id
+					},
+					{
+						header: 'More Info',
+						columns: [
+							{
+								accessorKey: 'parentCompany',
+								header: () => 'Company',
+								cell: (info) => {
+									if (info.getValue() !== null) {
+										const { name } = info.getValue()
+										return name
+									}
+									return 'N/A'
+								},
+								footer: (props) => props.column.id
+							},
+							{
+								accessorKey: '_id',
+								header: () => 'Dummy Data',
+								cell: () => {
+									return 'N/A'
+								},
+								footer: (props) => props.column.id
+							}
+						]
+					}
+				]
+			}
+		],
+		[]
+	)
+
+	const table = useReactTable({
+		data,
+		columns,
+		state: {
+			expanded
+		},
+		onExpandedChange: setExpanded,
+		getSubRows: (row) => row.subRows,
+		getCoreRowModel: getCoreRowModel(),
+		getPaginationRowModel: getPaginationRowModel(),
+		getFilteredRowModel: getFilteredRowModel(),
+		getExpandedRowModel: getExpandedRowModel(),
+		debugTable: true
+	})
 
 	return (
 		<section className="w-2/3">
@@ -148,7 +281,179 @@ const ReportingPage = ({ data, companyData }) => {
 					</tbody>
 				</table>
 			</div>
+			<div>
+				<table className="w-full">
+					<thead>
+						{table.getHeaderGroups().map((headerGroup) => (
+							<tr key={headerGroup.id}>
+								{headerGroup.headers.map((header) => {
+									return (
+										<th
+											key={header.id}
+											colSpan={header.colSpan}
+										>
+											{header.isPlaceholder ? null : (
+												<div>
+													{flexRender(header.column.columnDef.header, header.getContext())}
+													{header.column.getCanFilter() ? (
+														<div>
+															<Filter
+																column={header.column}
+																table={table}
+															/>
+														</div>
+													) : null}
+												</div>
+											)}
+										</th>
+									)
+								})}
+							</tr>
+						))}
+					</thead>
+					<tbody>
+						{table.getRowModel().rows.map((row) => {
+							return (
+								<tr
+									key={row.id}
+									className="border-b"
+								>
+									{row.getVisibleCells().map((cell) => {
+										return <td key={cell.id}>{flexRender(cell.column.columnDef.cell, cell.getContext())}</td>
+									})}
+								</tr>
+							)
+						})}
+					</tbody>
+				</table>
+				<div className="flex items-center gap-2">
+					{table.getRowModel().rows.length >= table.getState().pagination.pageSize ? (
+						<>
+							<button
+								className="border rounded p-1"
+								onClick={() => table.setPageIndex(0)}
+								disabled={!table.getCanPreviousPage()}
+							>
+								{'<<'}
+							</button>
+							<button
+								className="border rounded p-1"
+								onClick={() => table.previousPage()}
+								disabled={!table.getCanPreviousPage()}
+							>
+								{'<'}
+							</button>
+							<button
+								className="border rounded p-1"
+								onClick={() => table.nextPage()}
+								disabled={!table.getCanNextPage()}
+							>
+								{'>'}
+							</button>
+							<button
+								className="border rounded p-1"
+								onClick={() => table.setPageIndex(table.getPageCount() - 1)}
+								disabled={!table.getCanNextPage()}
+							>
+								{'>>'}
+							</button>
+							<span className="flex items-center gap-1">
+								<div>Page</div>
+								<strong>
+									{table.getState().pagination.pageIndex + 1} of {table.getPageCount()}
+								</strong>
+							</span>
+							<span className="flex items-center gap-1">
+								| Go to page:
+								<input
+									type="number"
+									defaultValue={table.getState().pagination.pageIndex + 1}
+									onChange={(e) => {
+										const page = e.target.value ? Number(e.target.value) - 1 : 0
+										table.setPageIndex(page)
+									}}
+									className="border p-1 rounded w-16"
+								/>
+							</span>
+							<select
+								value={table.getState().pagination.pageSize}
+								onChange={(e) => {
+									table.setPageSize(Number(e.target.value))
+								}}
+							>
+								{[10, 20, 30, 40, 50].map((pageSize) => (
+									<option
+										key={pageSize}
+										value={pageSize}
+									>
+										Show {pageSize}
+									</option>
+								))}
+							</select>
+							<div>{table.getRowModel().rows.length} Rows</div>
+							<pre>{JSON.stringify(expanded, null, 2)}</pre>
+						</>
+					) : null}
+				</div>
+			</div>
 		</section>
+	)
+}
+
+function Filter({ column, table }) {
+	const firstValue = table.getPreFilteredRowModel().flatRows[0]?.getValue(column.id)
+
+	const columnFilterValue = column.getFilterValue()
+
+	console.log({
+		value: firstValue,
+		type: typeof firstValue
+	})
+
+	return typeof firstValue === 'number' ? (
+		<div className="flex space-x-2">
+			<input
+				type="number"
+				value={columnFilterValue?.[0] ?? ''}
+				onChange={(e) => column.setFilterValue((old) => [e.target.value, old?.[1]])}
+				placeholder={`Min`}
+				className="w-24 border shadow rounded"
+			/>
+			<input
+				type="number"
+				value={columnFilterValue?.[1] ?? ''}
+				onChange={(e) => column.setFilterValue((old) => [old?.[0], e.target.value])}
+				placeholder={`Max`}
+				className="w-24 border shadow rounded"
+			/>
+		</div>
+	) : (
+		<input
+			type="text"
+			value={columnFilterValue ?? ''}
+			onChange={(e) => column.setFilterValue(e.target.value)}
+			placeholder={`Search...`}
+			className="w-36 border shadow rounded"
+		/>
+	)
+}
+
+function IndeterminateCheckbox({ indeterminate, className = '', ...rest }) {
+	const ref = React.useRef(null)
+
+	React.useEffect(() => {
+		if (typeof indeterminate === 'boolean') {
+			ref.current.indeterminate = !rest.checked && indeterminate
+		}
+	}, [ref, indeterminate])
+
+	return (
+		<input
+			type="checkbox"
+			ref={ref}
+			className={className + ' cursor-pointer'}
+			{...rest}
+		/>
 	)
 }
 
