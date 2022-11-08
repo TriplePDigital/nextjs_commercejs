5642d00d00c948b37e162b1fc4d4d0ddb89bd42d
Daniel B. Papp <dpapp001@odu.edu>
2022-11-08 09:34:15 -0500

    :recycle: (missions): Converted data fetching scheme for the page Switched from getting all the data through SSR to using SWR with client-side data fetching. This introduces queries that can be used by SWR which for now are located in the same file as the fetcher query function itself.

--- pages/missions.js
+++ pages/missions.js
@@ -1,113 +1,110 @@
-/* eslint-disable no-unused-vars */
-import { Loader } from '../components/util'
-import { fetcher } from '@/util/fetcher'
-import { FaTrophy } from 'react-icons/fa'
-import { ListOfCourses, Webinar } from '../components/Courses'
-import { useContext, useEffect, useState } from 'react'
-import { UserContext } from '../pages/_app'
-import { getSession, useSession } from 'next-auth/client'
-import { useRouter } from 'next/router'
-import Streak from '@/components/Courses/Streak'
-import { nanoid } from 'nanoid'
-import getEnrollmentByStudentID from '@/util/getEnrollmentByStudentID'
-import getTracks from '@/util/getTracks'
-import getUserFromSession from '@/util/getUserFromSession'
+import {Loader} from '../components/util'
+import {ListOfCourses} from '../components/Courses'
+import {useContext} from 'react'
+import {UserContext} from './_app'
+import {useSession} from 'next-auth/client'
+import  {enrollmentQuery} from '@/util/getEnrollmentByStudentID'
+import {trackQuery} from '@/util/getTracks'
+import useSWR from "swr";
+import getter from "@/util/getter";
+import {webinarQuery} from "@/util/getWebinar";
 
-export default function Course({ webinar, tracks, enrollment }) {
-	/* pulling user's context from _app */
-	const { user } = useContext(UserContext)
-	const [session, loading] = useSession()
-	const router = useRouter()
+export default function Course({}) {
+    /* pulling user's context from _app */
+    const {user} = useContext(UserContext)
+    const [session, loading] = useSession()
 
-	// if (!session) {
-	// 	return router.push('/auth/login')
-	// }
+    const {data: webinar, error: webinarError} = useSWR(webinarQuery, getter)
 
-	return loading ? (
-		<Loader />
-	) : (
-		<div className="flex flex-row w-full mt-5">
-			<section className="w-full">
-				{/* <Streak user={user} /> */}
-				{session && (
-					<>
-						<h1 className="text-4xl font-semibold mt-5">My Courses</h1>
-						<div className="flex overflow-x-scroll pb-10">
-							<div className="flex flex-nowrap mt-2">
-								{enrollment?.length > 0
-									? enrollment.map((item, enrollmentIndex) => (
-											<ListOfCourses
-												key={enrollmentIndex}
-												index={enrollmentIndex}
-												course={item.course}
-												progress={false}
-											/>
-									  ))
-									: null}
-							</div>
-						</div>
-					</>
-				)}
-				{tracks.map((track, i) => {
-					return track.numCourses !== 0 ? (
-						<div key={i}>
-							<h1 className="text-4xl font-semibold mt-5">{track.name}</h1>
-							<div className="flex overflow-x-scroll pb-10">
-								<div className="flex flex-nowrap mt-2">
-									{track.missions
-										? track.missions.map((course, courseIndex) => (
-												<ListOfCourses
-													key={courseIndex}
-													index={courseIndex}
-													course={course}
-													progress={false}
-												/>
-										  ))
-										: null}
-								</div>
-							</div>
-						</div>
-					) : null
-				})}
-			</section>
-			{/* <Webinar webinar={webinar} /> */}
-		</div>
-	)
-}
+    const {data: enrollment, error: enrollmentError} = useSWR(enrollmentQuery(user?._id), getter)
 
-export async function getServerSideProps(ctx) {
-	const session = await getSession(ctx)
-	const webinar = await fetcher(`
-			*[_type == 'webinar' ] | order(releaseDateDesc){
-				...,
-				presenters[]->{
-					...,
-					avatar {
-						...,
-						asset ->
-					}
-				},
-			}[0]`)
-	const tracks = await getTracks()
-	if (session) {
-		const usr = await getUserFromSession(session?.user?.email)
+    const {data: tracks, error: trackError, isValidating: isValidatingTracks} = useSWR(trackQuery, getter)
 
-		const enrollment = await getEnrollmentByStudentID(usr?._id)
+    if (!webinar || !enrollment || !tracks) return <Loader/>
 
-		return {
-			props: {
-				tracks,
-				webinar,
-				enrollment: enrollment?.length > 0 ? enrollment : []
-			}
-		}
-	} else {
-		return {
-			props: {
-				tracks,
-				webinar,
-				enrollment: []
-			}
-		}
-	}
+    if (webinarError || enrollmentError || trackError) return <div>Something went wrong</div>
+
+    const filterTracksFromEnrollment = (tracks, enrollment) =>{
+        // for each track in the array of tracks get the missions array property
+        // for each mission in the array of missions get the missionID property
+        // for each missionID in the array of missionIDs check if it is in the array of enrollment
+        // if it is in the array of enrollment then add the track to the array of filtered tracks
+        // return the array of filtered tracks
+
+        const enrolledMissions = enrollment.map(enrollment => enrollment.course._id)
+
+
+        let filteredTracks = tracks.map(track => {
+            if(track.missions && track.missions.length > 0){
+                let missions = track.missions.map(mission => mission._id)
+                //for each mission _id in the missions array check if it is in the enrolledMissions array
+                //if it is in the enrolledMissions array then remove it from the missions array
+
+                // holds the non-enrolled mission IDs
+                missions = missions.filter(mission => !enrolledMissions.includes(mission))
+
+                // holds all the track minus the missions that the student is enrolled in
+                track.missions = missions.map(mission => {
+                    return track.missions.find(missionObj => missionObj._id === mission)
+                })
+
+                return track
+            }
+            else{
+                return []
+            }
+        })
+        return filteredTracks
+    }
+
+    return isValidatingTracks ? (
+        <Loader/>
+    ) : (
+        <div className="flex flex-row w-full mt-5">
+            <section className="w-full">
+                {/*<Streak user={user} />*/}
+                {!loading && session && (
+                    <>
+                        <h1 className="text-4xl font-semibold mt-5">My Courses</h1>
+                        <div className="flex overflow-x-scroll pb-10">
+                            <div className="flex flex-nowrap mt-2">
+                                {enrollment?.result?.length > 0
+                                    ? enrollment.result.map((item, enrollmentIndex) => (
+                                        <ListOfCourses
+                                            key={enrollmentIndex}
+                                            index={enrollmentIndex}
+                                            course={item.course}
+                                            progress={false}
+                                        />
+                                    ))
+                                    : null}
+                            </div>
+                        </div>
+                    </>
+                )}
+                {filterTracksFromEnrollment(tracks?.result, enrollment?.result).map((track, i) => {
+                    return track?.missions && track?.missions.length > 0 ? (
+                        <div key={i}>
+                            <h1 className="text-4xl font-semibold mt-5">{track.name}</h1>
+                            <div className="flex overflow-x-scroll pb-10">
+                                <div className="flex flex-nowrap mt-2">
+                                    {track.missions
+                                        ? track.missions.map((course, courseIndex) => (
+                                            <ListOfCourses
+                                                key={courseIndex}
+                                                index={courseIndex}
+                                                course={course}
+                                                progress={false}
+                                            />
+                                        ))
+                                        : null}
+                                </div>
+                            </div>
+                        </div>
+                    ) : null
+                })}
+            </section>
+            {/*<Webinar webinar={webinar.result} />*/}
+        </div>
+    )
 }
---
+++ util/getWebinar.js
@@ -0,0 +1,12 @@
+import groq from "groq";
+
+export const webinarQuery = groq`*[_type == 'webinar' ] | order(releaseDateDesc){
+				...,
+				presenters[]->{
+					...,
+					avatar {
+						...,
+						asset ->
+					}
+				},
+			}[0]`
