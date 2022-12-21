a982ef6609310b71e9726e9b6d85e95930ce47c2
Dániel B. Papp <dpapp@odu.edu>
2022-12-21 12:28:34 -0500

    :sparkles: (cart): Added refund policy to prior to checkout

--- pages/cart.js
+++ pages/cart.js
@@ -7,11 +7,13 @@ import getter from '@/util/getter'
 import { FaTrash } from 'react-icons/fa'
 import { Loader } from '@/components/util'
 import checkout from '@/util/checkout'
+import Link from 'next/link'
 
 const Cart = () => {
 	const ctx = useContext(cartContextObject)
 	const { data, error } = useSWR(`*[_type == "mission"]`, getter)
 	const [courses, setCourses] = useState([])
+	const [checked, setChecked] = useState(false)
 
 	const getCourseDataFromCart = () => {
 		let filtered = []
@@ -57,7 +59,12 @@ const Cart = () => {
 						className="border-b px-4 py-4 flex justify-between items-center"
 					>
 						<div className="w-3/5 flex flex-col">
-							<span className="">{item.title}</span>
+							<Link
+								passHref={true}
+								href={`/mission/${item.slug.current}`}
+							>
+								<a className="hover:underline cursor-pointer">{item.title}</a>
+							</Link>
 							<span className="text-gray-400 text-sm">SKU: {item.sku || '000'}</span>
 						</div>
 						<span className="w-1/5 flex gap-2">
@@ -90,10 +97,24 @@ const Cart = () => {
 			<div className="border-b px-4 py-4 w-full">
 				<span className="text-gray-400 text-right text-sm w-full inline-block">The total price for your order will be shown after you click checkout</span>
 			</div>
-			<div className="px-4 py-4 flex justify-end items-center">
+			<div className="px-4 py-4 flex justify-between items-center">
+				<label
+					htmlFor="policy_agree"
+					className="w-1/2 flex items-center gap-2 flex-row-reverse"
+				>
+					<span className="text-sm">Purchases of online or downloadable products/services are final and non-refundable. By checking this box, you recognize and agree all sales are final with no warranties once a course is accessed.</span>
+					<input
+						type="checkbox"
+						name="policy"
+						id="policy_agree"
+						defaultValue={checked ? 'true' : 'false'}
+						onChange={() => setChecked(!checked)}
+					/>
+				</label>
 				<button
-					className={`bg-blue-500 text-white px-4 py-2 rounded-md ${courses.length === 0 ? 'opacity-50 cursor-not-allowed' : ''}`}
+					className={`bg-blue-500 text-white px-4 py-2 rounded-md ${courses.length === 0 || !checked ? 'opacity-50 cursor-not-allowed' : ''}`}
 					onClick={() => checkout(convertCourseOjbectToCheckoutItem(courses))}
+					disabled={courses.length === 0 || !checked}
 				>
 					Checkout
 				</button>
