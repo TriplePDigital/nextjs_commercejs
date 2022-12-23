5bbb28f8f3b169551c33cf3855d6becde9603db1
Dániel B. Papp <dpapp@odu.edu>
2022-12-23 17:45:57 -0500

    :recycle: (TS): Renamed getReceipt endpoint

---
+++ pages/api/user/getReceipt.ts
@@ -0,0 +1,34 @@
+import axios from 'axios'
+import xml2js from 'xml2js'
+
+export default async function (req, res) {
+	if (req.method !== 'GET') {
+		res.status(405).json({ message: 'Method Not Allowed' })
+	} else {
+		const { transid } = await req.query
+
+		const payment = await axios.post(`https://sweetblinginc.transactiongateway.com/api/query.php?security_key=${process.env.NMI_SECRET_KEY}&transaction_id=${transid}`)
+
+		const parser = new xml2js.Parser()
+
+		const response = await parser.parseStringPromise(payment.data)
+
+		const { transaction } = response.nm_response
+
+		if (!transaction) {
+			res.status(400)
+			res.json({ message: 'Transaction not found' })
+		} else {
+			const data = transaction[0]
+
+			const response = {
+				transaction_id: data.transaction_id[0],
+				amount: data.action[0].amount[0],
+				product: data.product,
+				email: data.email[0],
+				name: `${data.first_name[0]} ${data.last_name[0]}`
+			}
+			res.status(200).json({ message: 'Transaction found', transaction: response })
+		}
+	}
+}
