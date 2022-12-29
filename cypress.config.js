9f27c2d829563c65ac97d90298b0446e3f88b8c2
Dániel B. Papp <dpapp@odu.edu>
2022-12-29 16:20:57 -0500

    :test_tube: (testing): Expanded configuration

--- cypress.config.js
+++ cypress.config.js
@@ -8,10 +8,15 @@ module.exports = defineConfig({
 			bundler: 'webpack'
 		}
 	},
-
 	e2e: {
+		baseUrl: 'http://localhost:3000',
 		setupNodeEvents(on, config) {
-			// implement node event listeners here
+			require('@cypress/code-coverage/task')(on, config)
+			// include any other plugin code...
+
+			// It's IMPORTANT to return the config object
+			// with any changed environment variables
+			return config
 		}
 	}
 })
